c58a987a8d853fad688d6076f217f584f9039ff7
911-Andrioaie-Daria <72091687+911-Andrioaie-Daria@users.noreply.github.com>
2022-03-30 19:00:40 +0300

    Assignment 2 completed

---
+++ Assignments/Assignment2/Lab1Enhanced/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Lab1Enhanced
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvChild = new System.Windows.Forms.DataGridView();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dgvParent = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRoutes
+            // 
+            this.dgvChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChild.Location = new System.Drawing.Point(595, 48);
+            this.dgvChild.Name = "dgvChild";
+            this.dgvChild.RowHeadersWidth = 51;
+            this.dgvChild.RowTemplate.Height = 24;
+            this.dgvChild.Size = new System.Drawing.Size(478, 338);
+            this.dgvChild.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(938, 466);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(123, 53);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // dgvStations
+            // 
+            this.dgvParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParent.Location = new System.Drawing.Point(65, 48);
+            this.dgvParent.Name = "dgvParent";
+            this.dgvParent.RowHeadersWidth = 51;
+            this.dgvParent.RowTemplate.Height = 24;
+            this.dgvParent.Size = new System.Drawing.Size(485, 338);
+            this.dgvParent.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(775, 466);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(123, 53);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 556);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvParent);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.dgvChild);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvChild;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dgvParent;
+        private System.Windows.Forms.Button btnUpdate;
+
+    }
+}
+
---
+++ Assignments/Assignment2/Lab1Enhanced/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace Lab1Enhanced
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source = DESKTOP-ERFTJDA; Initial Catalog = RailwayCompany; Integrated Security = True;");
+        DataSet dataSet = new DataSet();
+
+        SqlDataAdapter dataAdapterChild = new SqlDataAdapter();
+        SqlDataAdapter dataAdapterParent = new SqlDataAdapter();
+
+        BindingSource bindingSourceChild = new BindingSource();
+        BindingSource bindingSourceParent = new BindingSource();
+
+      
+        DataRelation childParentRelation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString.ToString();
+        }
+
+        private string getParentQuery()
+        {
+            return ConfigurationManager.AppSettings["parent_query"];
+        }
+
+        private string getChildQuery()
+        {
+            return ConfigurationManager.AppSettings["child_query"];
+        }
+
+        private string getParentTable()
+        {
+            return ConfigurationManager.AppSettings["parent_table"];
+        }
+
+        private string getChildTable()
+        {
+            return ConfigurationManager.AppSettings["child_table"];
+        }
+
+        private string getParentPKName()
+        {
+            return ConfigurationManager.AppSettings["parent_pk_column"];
+        }
+
+        private string getChildFKName()
+        {
+            return ConfigurationManager.AppSettings["child_fk_column"];
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // populate tables
+            dataAdapterChild.SelectCommand = new SqlCommand(getChildQuery(), connection);
+            dataAdapterChild.Fill(dataSet, getChildTable());
+
+
+            dataAdapterParent.SelectCommand = new SqlCommand(getParentQuery(), connection);
+            dataAdapterParent.Fill(dataSet, getParentTable());
+
+            // establish relation between tables
+            bindingSourceParent.DataSource = dataSet;
+            bindingSourceParent.DataMember = getParentTable();
+            dgvParent.DataSource = bindingSourceParent;
+
+            DataColumn parentPKColumn = dataSet.Tables[getParentTable()].Columns[getParentPKName()];
+            DataColumn childFKColumn = dataSet.Tables[getChildTable()].Columns[getChildFKName()];
+            childParentRelation = new DataRelation("FK_child_parent", parentPKColumn, childFKColumn);
+            dataSet.Relations.Add(childParentRelation);
+
+            bindingSourceChild.DataSource = bindingSourceParent;
+            bindingSourceChild.DataMember = "FK_child_parent";
+            dgvChild.DataSource = bindingSourceChild;
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(dataAdapterChild);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataAdapterChild.Update(dataSet, getChildTable());
+                MessageBox.Show("Update was successful!");
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
